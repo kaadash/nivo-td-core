e20249f9da85117879a44d703b7d90c6362e8c76
Artur Kudeł <akudel@apptension.com>
2017-12-13 12:09:55 +0100

    Add correct axis 0

--- src/components/axes/Axis.js
+++ src/components/axes/Axis.js
@@ -146,7 +146,7 @@ const Axis = ({
                         key={tick.key}
                         value={tick.key}
                         format={format}
-                        lineX={index === 0 && _position === 'left' ? width : tick.lineX}
+                        lineX={tick.key === 0 && _position === 'left' ? width : tick.lineX}
                         lineY={tick.lineY}
                         rotate={tickRotation}
                         textX={tick.textX}
